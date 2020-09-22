5f954aabd84e75f2d8dbcaa1e9949891582bf7d7
Filip Oščádal <f@mxd.cz>
2020-09-22 14:51:28 +0200

    minor update

--- Makefile
+++ Makefile
@@ -1,4 +1,7 @@
-all: docs
+all: info
+
+info:
+	@echo "🆘  make docs ➡️ rebuild *.adoc and *.PDF"
 
 docs:
 	@/bin/bash ./create_pdf.sh
--- create_pdf.sh
+++ create_pdf.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-find . -type f -iname "*.md" -exec docker run --rm -v "$(pwd)":/data pandoc/core -f markdown -t asciidoc -i {} -o {}.adoc \;
-find . -type f -iname "*.adoc" -exec docker run --rm -v $(pwd):/documents/ asciidoctor/docker-asciidoctor asciidoctor-pdf -a allow-uri-read -d book {} \;
+find . -type f -iname "*.md" -exec echo {} \; -exec docker run --rm -v "$(pwd)":/data pandoc/core -f markdown -t asciidoc -i {} -o {}.adoc \;
+find . -type f -iname "*.adoc" -exec echo {} \; -exec docker run --rm -v $(pwd):/documents/ asciidoctor/docker-asciidoctor asciidoctor-pdf -a allow-uri-read -d book {} \;
