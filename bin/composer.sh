6b26fa741dd00f91241de6809c1fcc81acfb7144
Filip Oščádal <f@mxd.cz>
2020-09-25 08:42:45 +0200

    minor update

--- bin/composer.sh
+++ bin/composer.sh
@@ -8,9 +8,9 @@ if [ "$(id -u)" != "0" ]; then fail "This script must be run as root!"; fi
 command -v php >/dev/null 2>&1 || fail "PHP is NOT installed!"
 
 info 'Installing Composer ...\n'
-sleep 2
 
 curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
-rm -rf ~/.composer/
+rm -rf ~/.composer
+
 composer -V
