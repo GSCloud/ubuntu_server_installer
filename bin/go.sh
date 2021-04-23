b650d7578d69d7b4e14620759d8ea8df091739bd
Filip Oščádal <f@mxd.cz>
2021-04-23 10:43:27 +0200

    + updated Go installer

--- bin/go.sh
+++ bin/go.sh
@@ -24,6 +24,6 @@ tar -C /usr/local -xzf $T
 
 yes_or_no "Remove temporary file?" && rm -f $T
 
-info 'Remember to add: "export PATH=$PATH:/usr/local/go/bin;export GOROOT=/usr/local/go;export GOPATH=$HOME/go" to your profile!'
+info "Remember to add these lines to your profile:\n\nexport PATH=$PATH:/usr/local/go/bin\nexport GOROOT=/usr/local/go\nexport GOPATH=$HOME/go"
 
 exit 0
