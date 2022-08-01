5697369813580b09f75bf6d24889df046eda00a8
Filip Oščádal <f@mxd.cz>
2022-08-01 07:40:01 +0200

    + Golang 1.18.4

--- bin/go.sh
+++ bin/go.sh
@@ -9,9 +9,9 @@ command -v wget >/dev/null 2>&1 || fail "wget is NOT installed!"
 
 info 'Downloading Go'
 
-T="/tmp/go1.18.3.tar.gz"
+T="/tmp/go1.18.4.tar.gz"
 if [ ! -f "$T" ]; then
-  wget -O $T 'https://dl.google.com/go/go1.18.3.linux-amd64.tar.gz'
+  wget -O $T 'https://dl.google.com/go/go1.18.4.linux-amd64.tar.gz'
 fi
 
 info 'Removing previous Go'
