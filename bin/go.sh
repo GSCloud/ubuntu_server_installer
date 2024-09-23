188f3477eb15e780444e61cb5857b0ca27dd1048
Filip Oščádal <f@mxd.cz>
2024-09-23 09:08:56 +0200

    go v1.23.1

--- bin/go.sh
+++ bin/go.sh
@@ -9,9 +9,9 @@ command -v wget >/dev/null 2>&1 || fail "wget is NOT installed!"
 
 info 'Downloading Go'
 
-T="/tmp/go1.22.1.tar.gz"
+T="/tmp/go1.23.1.tar.gz"
 if [ ! -f "$T" ]; then
-  wget -O $T 'https://dl.google.com/go/go1.22.1.linux-amd64.tar.gz'
+  wget -O $T 'https://dl.google.com/go/go1.23.1.linux-amd64.tar.gz'
 fi
 
 info 'Removing previous Go'
