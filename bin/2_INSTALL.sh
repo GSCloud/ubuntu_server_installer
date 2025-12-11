4e46ddffb9b0db4ff6f5632fd6ba034f3d277798
Fred Brooker <git@gscloud.cz>
2025-12-11 14:41:17 +0100

    minor update

--- bin/2_INSTALL.sh
+++ bin/2_INSTALL.sh
@@ -17,7 +17,7 @@ info 'Installing basic packages'
 LC_ALL=en_US.UTF-8 add-apt-repository -y ppa:git-core/ppa
 apt-get install -y apache2 apache2-dev curl dkms git-core gitk haveged htop iptables \
   language-pack-en language-pack-en-base man mc nano ntp openssl p7zip software-properties-common \
-  traceroute tree ufw whois
+  traceroute tree ufw whois jq parallel rename
 
 info 'Updating APT'
 
